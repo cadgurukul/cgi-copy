63bce3fffb7bb0b4835f8faa1ff424c9b16cd12d
Adish <cadgurukul01@gmail.com>
2026-01-01 12:17:31 +0530

    Update

--- frontend/src/pages/TeamPage.jsx
+++ frontend/src/pages/TeamPage.jsx
@@ -50,7 +50,7 @@ const TeamPage = () => {
             </p>
           </div>
 
-          <div className="grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8">
+          <div className="grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 gap-8">
             {teamMembers.map((member) => (
               <div
                 key={member.id}
@@ -61,7 +61,7 @@ const TeamPage = () => {
                   <img
                     src={member.image}
                     alt={member.name}
-                    className="w-full h-full object-cover group-hover:scale-105 transition-transform duration-300"
+                    className="w-full object-cover group-hover:scale-105 transition-transform duration-300"
                   />
                 </div>
 
